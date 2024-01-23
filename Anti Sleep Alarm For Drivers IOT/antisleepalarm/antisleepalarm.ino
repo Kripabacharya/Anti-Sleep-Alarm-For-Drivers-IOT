0ee25179d586079a20cb42870206f2bc327db54d
Kripa B Acharya <123444380+Kripabacharya@users.noreply.github.com>
2024-01-23 20:08:18 +0530

    Update antisleepalarm.ino

--- Anti Sleep Alarm For Drivers IOT/antisleepalarm/antisleepalarm.ino
+++ Anti Sleep Alarm For Drivers IOT/antisleepalarm/antisleepalarm.ino
@@ -1,45 +1,35 @@
 #define Relay 12
 #define buzzer A0
-static const int sensorPin = 10;                    // sensor input pin 
-int SensorStatePrevious = LOW;                      // previousstate of the sensor
+static const int sensorPin = 10;                    
+int SensorStatePrevious = LOW;                  
 
-unsigned long minSensorDuration = 3000;           // Time we wait before  the sensor active as long 
+unsigned long minSensorDuration = 3000;            
 unsigned long minSensorDuration2 = 6000;
-unsigned long SensorLongMillis;                // Time in ms when the sensor was active
-bool SensorStateLongTime = false;                  // True if it is a long active
+unsigned long SensorLongMillis;                
+bool SensorStateLongTime = false;                  
 
-const int intervalSensor = 50;                      // Time between two readings sensor state
-unsigned long previousSensorMillis;                 // Timestamp of the latest reading
+const int intervalSensor = 50;                    
+unsigned long previousSensorMillis;                 
 
-unsigned long SensorOutDuration;                  // Time the sensor is active in ms
-
-//// GENERAL ////
-
-unsigned long currentMillis;          // Variabele to store the number of milleseconds since the Arduino has started
+unsigned long SensorOutDuration;                 
+unsigned long currentMillis;        
 
 void setup() {
-  Serial.begin(9600);                 // Initialise the serial monitor
+  Serial.begin(9600);                
 
-  pinMode(sensorPin, INPUT);          // set sensorPin as input
+  pinMode(sensorPin, INPUT);         
   Serial.println("Press button");
   pinMode(Relay,OUTPUT);
   pinMode(buzzer,OUTPUT);
 }
 
-// Function for reading the sensor state
+
 void readSensorState() {
 
-  // If the difference in time between the previous reading is larger than intervalsensor;
-
-  
   if(currentMillis - previousSensorMillis > intervalSensor) {
-    
-    // Read the digital value of the sensor (LOW/HIGH)
     int SensorState = digitalRead(sensorPin);    
     Serial.println(SensorState);
-    // If the button has been active AND
-    // If the sensor wasn't activated before AND
-    // IF there was not already a measurement running to determine how long the sensor has been activated
+    
     if (SensorState == HIGH && SensorStatePrevious == HIGH && !SensorStateLongTime) {
      SensorLongMillis = currentMillis;
        SensorStatePrevious = LOW;
@@ -47,12 +37,9 @@ void readSensorState() {
       Serial.println("Button pressed");
     }
 
-    // Calculate how long the sensor has been activated
+  
    SensorOutDuration = currentMillis - SensorLongMillis;
 
-    // If the button is active AND
-    // If there is no measurement running to determine how long the sensor is active AND
-    // If the time the sensor has been activated is larger or equal to the time needed for a long active
     if (SensorState == HIGH && !SensorStateLongTime && SensorOutDuration >= minSensorDuration) {
       SensorStateLongTime = true;
       digitalWrite(buzzer,HIGH);
@@ -65,8 +52,6 @@ void readSensorState() {
       Serial.println("Button long pressed");
     }
       
-    // If the sensor is released AND
-    // If the sensor was activated before
     if (SensorState == LOW && SensorStatePrevious == LOW) {
       SensorStatePrevious = HIGH;
       SensorStateLongTime = false;
@@ -77,7 +62,6 @@ void readSensorState() {
   
     }
     
-    // store the current timestamp in previousSensorMillis
    previousSensorMillis = currentMillis;
 
   }
@@ -86,8 +70,8 @@ void readSensorState() {
 
 void loop() {
 
-  currentMillis = millis();    // store the current time
-  readSensorState();           // read the sensor state
+  currentMillis = millis();    
+  readSensorState();           
   
 }
 
